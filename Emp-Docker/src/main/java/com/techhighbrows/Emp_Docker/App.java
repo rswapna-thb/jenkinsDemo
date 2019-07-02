e60f1f21362aab3ed8c387afd35e94fbfbced241
Reningutla Swapna <rswapna@techhighbrows.com>
2019-07-02 10:53:13 +0530

    dummy

---
+++ Emp-Docker/src/main/java/com/techhighbrows/Emp_Docker/App.java
@@ -0,0 +1,13 @@
+package com.techhighbrows.Emp_Docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
